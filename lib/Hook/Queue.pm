87ed7839876b6d61ee13b043eb8bfd54c1a44159
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-02-18 23:59:57 +0000

    skeletons
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Hook-Queue@2156 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Hook/Queue.pm
@@ -0,0 +1,49 @@
+package Hook::Queue;
+
+use strict;
+#use warnings;
+
+
+
+=head1 NAME
+
+Hook::Queue - 
+
+=head1 SYNOPSIS
+
+  use Hook::Queue;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+Copyright Richard Clamp 2004.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+=head1 BUGS
+
+None known.
+
+Bugs should be reported to me via the CPAN RT system.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Hook::Queue>.
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
